3f0c92a7270b75af21171541db27a58375187a22
Miguel Marco Valencia <miguel.valencia@uq.edu.au>
2022-07-31 18:24:37 +1000

    Added RGB and gain control

--- ximea_ros/src/ximea_ros.cpp
+++ ximea_ros/src/ximea_ros.cpp
@@ -1,10 +1,8 @@
 #include <iostream>
 #include <ximea_cv/ximea_cv.hpp>
+#include <ximea_cv/ximea_ros.hpp>
+#include <signal.h>
 
-#include <ximea_cv/ximea_cv.hpp>
-
-using namespace cv;
-using namespace std;
 
 XimeaROS::XimeaROS(int argc, char** argv) {
     ros::init(argc, argv, "ximea_camera");
@@ -15,71 +13,113 @@ XimeaROS::XimeaROS(int argc, char** argv) {
  * 
  * @param serials List of serial numbers to initialise
  */
-void KinectROS::init_camera_pub(std::vector<std::string> serials) {
+void XimeaROS::init_camera_pub(std::vector<std::string> serials) {
     // For each device, create node handles and publishers.
     for (std::string serial : serials) {
 
-        nh_camera_info_colors[serial]   = std::make_unique<ros::NodeHandle>();
-        nh_camera_info_irs[serial]      = std::make_unique<ros::NodeHandle>();
+        nh_camera_info[serial]	= std::make_unique<ros::NodeHandle>();
+        nh_camera[serial]		= std::make_unique<ros::NodeHandle>(std::string("ximea_") + serial);
+        pub_camera_info[serial]	= nh_camera_info[serial]->advertise<sensor_msgs::CameraInfo>(std::string("ximea_") + serial + std::string("/camera_info"), 1);
+        camera_info_manager[serial]	= std::make_unique<camera_info_manager::CameraInfoManager>(*nh_camera[serial], std::string("ximea_") + serial);
 
-        nh_camera_colors[serial]        = std::make_unique<ros::NodeHandle>(std::string("color_") + serial);
-        nh_camera_irs[serial]           = std::make_unique<ros::NodeHandle>(std::string("ir_") + serial);
-
-        pub_camera_info_colors[serial]  = nh_camera_info_colors[serial]->advertise<sensor_msgs::CameraInfo>(std::string("color_") + serial + std::string("/camera_info"), 1);
-        pub_camera_info_irs[serial]     = nh_camera_info_irs[serial]->advertise<sensor_msgs::CameraInfo>(std::string("ir_") + serial + std::string("/camera_info"), 1);
-
-        // For each device, create a camera info manager using the default directory for calibration data.
-        manager_colors[serial]          = std::make_unique<camera_info_manager::CameraInfoManager>(*nh_camera_colors[serial], std::string("color_") + serial);
-        manager_irs[serial]             = std::make_unique<camera_info_manager::CameraInfoManager>(*nh_camera_irs[serial], std::string("ir_") + serial);
-
-        if (!manager_colors[serial]->loadCameraInfo("")) {
-            std::cout << "Failed to get calibration for color camera: " << serial << std::endl;
+        if (!camera_info_manager[serial]->loadCameraInfo("")) {
+            std::cout << "Failed to get calibration for camera: " << serial << std::endl;
         }
 
-        if (!manager_irs[serial]->loadCameraInfo("")) {
-            std::cout << "Failed to get calibration from ir camera: " << serial << std::endl;
-        }
+        camera_info[serial] = camera_info_manager[serial]->getCameraInfo();
+    }
+}
 
-        info_colors[serial] = manager_colors[serial]->getCameraInfo();
-        info_irs[serial]    = manager_irs[serial]->getCameraInfo();
+void XimeaROS::init_img_pub(std::vector<std::string> serials) {
+    for (std::string serial : serials) {
+
+        it_img[serial]   = std::make_unique<image_transport::ImageTransport>(ros::NodeHandle()); // Image Transport Node Handle for Color Frame
+        
+        pub_img[serial]  = it_img[serial]->advertise(std::string("ximea_") + serial + "/image_raw", 1);               // Publisher for Color Frame
     }
 }
 
 
+void XimeaROS::send_image(std::string serial, cv::Mat img, std::string format) {
+    // Create ROS sensor messages from OpenCV Images
+    sensor_msgs::ImagePtr msg = cv_bridge::CvImage(std_msgs::Header(), format, img).toImageMsg();
+
+    // Publish the sensor messages
+    pub_img[serial].publish(msg);
+
+    // Create and publish the camera info
+    sensor_msgs::CameraInfo info_camera = camera_info_manager[serial]->getCameraInfo();
+    ros::Time now = ros::Time::now();
+    info_camera.header.stamp = now;
+    pub_camera_info[serial].publish(info_camera);
+}
+
 int main(int argc, char** argv) {
-	try {
-		// Sample for XIMEA OpenCV
-		xiAPIplusCameraOcv cam;
+	XimeaROS ximea_ros(argc, argv);
+	std::vector<std::string> serials = {
+		"31702951"
+	};
+	ximea_ros.init_camera_pub(serials);
+	ximea_ros.init_img_pub(serials);
 
-		// Retrieving a handle to the camera device
-		cout << "Opening first camera..." << endl;
-		cam.OpenFirst();
-		
-		// Set exposure
-		cam.SetExposureTime(5000);
-		cam.SetGain(12.0);
-		cam.SetDownsampling((XI_DOWNSAMPLING_VALUE)2);
-		cam.SetImageDataFormat(XI_MONO8);
-		
-		cout << "Starting acquisition..." << endl;
-		cam.StartAcquisition();
-		
-		while (true) {
-			Mat cv_mat_image = cam.GetNextImageOcvMat();
-    		imshow("Image from camera", cv_mat_image);
-			waitKey(1);
+	// Sample for XIMEA OpenCV
+	std::map<std::string, std::unique_ptr<xiAPIplusCameraOcv>> cam;
+	for (std::string serial : serials) {
+		cam[serial] = std::make_unique<xiAPIplusCameraOcv>();
+		cam[serial]->OpenBySN(serial.c_str());
+	}
+	float exposure_time = 6000.0;
+	float gain = 0.0;
+	for (std::string serial : serials) {
+		cam[serial]->SetExposureTime(exposure_time);
+		cam[serial]->SetGain(gain);
+		cam[serial]->SetDownsampling((XI_DOWNSAMPLING_VALUE)2);
+		cam[serial]->SetImageDataFormat(XI_MONO8);
+	}
+	
+	for (std::string serial : serials) {
+		cam[serial]->StartAcquisition();
+	}
+	bool rgb = false;
+	std::string format = "mono8";
+	while (ros::ok()) {
+		for (std::string serial : serials) {
+			cv::Mat img = cam[serial]->GetNextImageOcvMat();
+			ximea_ros.send_image(serial, img, format);
+			cv::imshow(serial, img);
+			char c = cv::waitKey(1);
+			if (c == '=') {
+				gain += 0.5;
+				if (gain > 15.0) {
+					gain = 15.0;
+				}
+				cam[serial]->SetGain(gain);
+			} else if (c == '-') {
+				gain -= 0.5;
+				if (gain < 0.0) {
+					gain = 0.0;
+				}
+				cam[serial]->SetGain(gain);
+			} else if (c == ' ') {
+				rgb = !rgb;
+				if (!rgb) {
+					cam[serial]->SetImageDataFormat(XI_MONO8);
+					format = "mono8";
+				} else {
+					cam[serial]->SetImageDataFormat(XI_RGB24);
+					format = "bgr8";
+				}
+			}
+			cv::waitKey(1);
 		}
-		
-		cam.StopAcquisition();
-		cam.Close();
-		cout << "Done" << endl;
-		waitKey(1000);
-	} catch(xiAPIplus_Exception& exp) {
-
-		cout << "Error:" << endl;
-		exp.PrintError();
-		waitKey(3000);
-		return -1;
+	}
+	
+	for (std::string serial : serials) {
+		cam[serial]->StopAcquisition();
+	}
+	
+	for (std::string serial : serials) {
+		cam[serial]->Close();
 	}
 	return 0;
 }
